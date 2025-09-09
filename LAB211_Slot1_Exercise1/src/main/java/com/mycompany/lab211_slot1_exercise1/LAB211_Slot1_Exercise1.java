6414719b7f258055cfe8183bcb15c57b623bbde6
XcaLLix <136565480+XcaLLix@users.noreply.github.com>
2025-09-09 16:45:09 +0700

    Add files via upload

--- LAB211_Slot1_Exercise1/src/main/java/com/mycompany/lab211_slot1_exercise1/LAB211_Slot1_Exercise1.java
+++ LAB211_Slot1_Exercise1/src/main/java/com/mycompany/lab211_slot1_exercise1/LAB211_Slot1_Exercise1.java
@@ -10,31 +10,17 @@ package com.mycompany.lab211_slot1_exercise1;
  */
 
 import java.util.*;
+
+
 public class LAB211_Slot1_Exercise1 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your content: ");
         String input = sc.nextLine();
-        
-        // ---- Đếm từ ----
-        StringTokenizer tokenizer = new StringTokenizer(input);
-        
-        Map<String, Integer> wordCount = new LinkedHashMap<>();
-        while (tokenizer.hasMoreTokens()) {
-            String word = tokenizer.nextToken();
-            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
-        }
+        Count count = new Count(input);
 
-        // ---- Đếm ký tự ----
-        Map<Character, Integer> charCount = new LinkedHashMap<>();
-        for (char c : input.toCharArray()) {
-            if (c != ' ') { 
-                charCount.put(c, charCount.getOrDefault(c, 0) + 1);
-            }
-        }
-
-        System.out.println(wordCount);
-        System.out.println(charCount);
+        System.out.println(count.wordCount());
+        System.out.println(count.charCount());
     }
 }
