f10bc620bedd45574bdf2b4dafc350df7bb92029
XcaLLix <136565480+XcaLLix@users.noreply.github.com>
2025-10-18 22:58:37 +0700

    Add files via upload

---
+++ slot_6/LAB211_Slot6_Exercise/src/main/java/com/mycompany/lab211_slot6_exercise/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab211_slot6_exercise;
+
+
+/**
+ *
+ * @author xcall
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        FruitShop shop = new FruitShop();
+        shop.run();
+    }
+   
+}
